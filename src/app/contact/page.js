a19746983f78ea67fe1785e60e54acd9e007a1c8
faishaltsq <cubebbc@gmail.com>
2025-07-18 21:47:47 +0700

    update

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -75,7 +75,7 @@ export default function ContactPage() {
               rel='noopener noreferrer'
               className='text-sky-900 hover:underline underline'
             >
-              +62 811-7784-099 (Ahdi Fitriyanti)
+              +62 811-7784-099 (Halo Monsiska MI)
             </a>
           </div>
           <div className='flex flex-col items-center'>
